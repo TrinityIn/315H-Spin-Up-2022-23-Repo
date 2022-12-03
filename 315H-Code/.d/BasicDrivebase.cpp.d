c7c94cb2fce97c71c72dce0685ed1c06e8306898
Evan Wu <46251676+TrinityIn@users.noreply.github.com>
2022-12-03 14:47:12 -0800

    current code upload

--- 315H-Code/.d/BasicDrivebase.cpp.d
+++ 315H-Code/.d/BasicDrivebase.cpp.d
@@ -1,17 +1,17 @@
-bin/main.cpp.o: src/main.cpp include/main.h include/api.h \
- include/pros/adi.h include/pros/colors.h include/pros/distance.h \
- include/pros/error.h include/pros/ext_adi.h include/pros/adi.h \
- include/pros/adi.h include/pros/gps.h include/pros/imu.h \
- include/pros/link.h include/pros/llemu.h include/display/lvgl.h \
- include/display/lv_version.h include/display/lv_misc/lv_log.h \
- include/display/lv_conf.h include/display/lv_conf_checker.h \
- include/display/lv_misc/lv_task.h include/display/lv_misc/lv_mem.h \
- include/display/lv_misc/lv_log.h include/display/lv_misc/lv_ll.h \
- include/display/lv_hal/lv_hal.h include/display/lv_hal/lv_hal_disp.h \
- include/display/lv_hal/lv_hal.h include/display/lv_misc/lv_color.h \
- include/display/lv_misc/lv_area.h include/display/lv_hal/lv_hal_indev.h \
- include/display/lv_core/lv_obj.h include/display/lv_core/lv_style.h \
- include/display/lv_misc/lv_font.h \
+bin/BasicDrivebase.cpp.o: src/BasicDrivebase.cpp include/main.h \
+ include/api.h include/pros/adi.h include/pros/colors.h \
+ include/pros/distance.h include/pros/error.h include/pros/ext_adi.h \
+ include/pros/adi.h include/pros/adi.h include/pros/gps.h \
+ include/pros/imu.h include/pros/link.h include/pros/llemu.h \
+ include/display/lvgl.h include/display/lv_version.h \
+ include/display/lv_misc/lv_log.h include/display/lv_conf.h \
+ include/display/lv_conf_checker.h include/display/lv_misc/lv_task.h \
+ include/display/lv_misc/lv_mem.h include/display/lv_misc/lv_log.h \
+ include/display/lv_misc/lv_ll.h include/display/lv_hal/lv_hal.h \
+ include/display/lv_hal/lv_hal_disp.h include/display/lv_hal/lv_hal.h \
+ include/display/lv_misc/lv_color.h include/display/lv_misc/lv_area.h \
+ include/display/lv_hal/lv_hal_indev.h include/display/lv_core/lv_obj.h \
+ include/display/lv_core/lv_style.h include/display/lv_misc/lv_font.h \
  include/display/lv_misc/lv_symbol_def.h \
  include/display/lv_fonts/lv_font_builtin.h \
  include/display/lv_misc/lv_anim.h include/display/lv_misc/lv_mem.h \
@@ -78,7 +78,8 @@ bin/main.cpp.o: src/main.cpp include/main.h include/api.h \
  include/pros/vision.h include/pros/link.hpp include/pros/link.h \
  include/RobotConfig.hpp include/AutonPrograms.hpp \
  include/DrivebaseControl.hpp include/CatapultControl.hpp \
- include/RollerControl.hpp
+ include/RollerControl.hpp include/BasicDrivebase.hpp \
+ include/pros/motors.hpp
 include/main.h:
 include/api.h:
 include/pros/adi.h:
@@ -237,3 +238,5 @@ include/AutonPrograms.hpp:
 include/DrivebaseControl.hpp:
 include/CatapultControl.hpp:
 include/RollerControl.hpp:
+include/BasicDrivebase.hpp:
+include/pros/motors.hpp:
